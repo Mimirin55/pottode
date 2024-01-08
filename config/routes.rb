7fb2a3295b219c6bebbb5e5ffad2ba1a7cfb14d0
Mimirin55 <178504.hinotori@gmail.com>
2024-01-08 19:39:02 +0900

    deviseログイン機能実装（新規登録・ログイン・編集・ログアウト）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root "tasks#index"
+  devise_for :users
+  devise_scope :users do
+    get '/users', to: redirect("/users/sign_up")
+    delete '/users' => 'devise/registrations#destroy', as: :destroy_user_registration
+  end
   resources :tasks
+  root "tasks#index"
 end
--- db/migrate/20240107071112_create_tasks.rb
+++ db/migrate/20240107071112_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-
+      t.string :title, limit: 30, null: false
+      t.text :description
       t.timestamps
     end
   end
