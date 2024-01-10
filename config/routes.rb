5fc7932ca365a12c0a0be5da80ad567901bcc512
Mimirin55 <178504.hinotori@gmail.com>
2024-01-10 15:05:20 +0900

    デプロイ環境整え他

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'tasks#index'
   devise_for :users
   devise_scope :users do
-    get '/users', to: redirect("/users/sign_up")
     delete '/users' => 'devise/registrations#destroy', as: :destroy_user_registration
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2024_01_09_022125) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
